68d24085ff67cbb328101410127e4d07a1a5ce2e
Mehar <mehar@Atenders-iMac.local>
2021-09-09 10:37:19 +0100

    added validtors

--- applications/test/test_app.py
+++ applications/test/test_app.py
@@ -1,1 +1,17 @@
 from flask_testing import TestCase
+from applications import app, db
+from applications.models import Questions, Options
+
+
+class TestBase(TestCase):
+    def create_app(self):
+
+        # Pass in testing configurations for the app.
+        app.config.update(SQLALCHEMY_DATABASE_URI="sqlite:///Patientdata.db",
+                SECRET_KEY='TEST_SECRET_KEY',
+                DEBUG=True,
+                WTF_CSRF_ENABLED=False
+                )
+        return app
+
+    
